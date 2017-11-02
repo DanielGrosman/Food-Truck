a754c933e82af51b573d99bb37b3586e97910a46
Daniel Grosman <danny.grosman@gmail.com>
2017-11-02 16:26:38 -0400

    first commit

---
+++ Foodtruck/Cook.h
@@ -0,0 +1,13 @@
+//
+//  Cook.h
+//  Foodtruck
+//
+//  Created by Daniel Grosman on 2017-11-02.
+//  Copyright © 2017 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cook : NSObject
+
+@end
---
+++ Foodtruck/Cook.m
@@ -0,0 +1,13 @@
+//
+//  Cook.m
+//  Foodtruck
+//
+//  Created by Daniel Grosman on 2017-11-02.
+//  Copyright © 2017 Lighthouse Labs. All rights reserved.
+//
+
+#import "Cook.h"
+
+@implementation Cook
+
+@end
